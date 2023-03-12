a55087994779503f4cfef3c3cb261279a9456e86
Emerson Espinoza <xinayespinoza@gmail.com>
2023-03-12 14:29:05 -0300

    34 protegiendo nuestras paginas

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    login
+  end
   test 'render a list of products' do
     get products_path
 
