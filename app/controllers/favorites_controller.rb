b17b089f0e135305cbc0fbdc91b2c1fea57dc0e4
Emerson Espinoza <xinayespinoza@gmail.com>
2023-04-01 01:49:46 -0300

    41 quitando favoritos

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    Favorite.create(product: product, user: Current.user)
+    product.favorite!
     redirect_to product_path(product)
   end
 
+  def destroy
+    product.unfavorite!
+    redirect_to product_path(product), status: :see_other
+  end
+  
   private
-
   def product
     @product ||= Product.find(params[:product_id])
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,25 +1,28 @@
 class Product < ApplicationRecord
   include PgSearch::Model
+  include Favoritable
+
   pg_search_scope :search_full_text, against: {
     title: 'A',
     description: 'B'
   }
+
   ORDER_BY = {
     newest: "created_at DESC",
     expensive: "price DESC",
-    cheaper: "price ASC"
-      # para filtrar todo el diccionario 
+    cheapest: "price ASC"
   }
+
   has_one_attached :photo
+
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true
-  has_many :favorites, dependent: :destroy
+
   belongs_to :category
-  belongs_to :user, default: -> {Current.user}
+  belongs_to :user, default: -> { Current.user }
 
   def owner?
     user_id == Current.user&.id
   end
-
 end
