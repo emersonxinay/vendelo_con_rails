fb2959944f7800eebb7d5ddbefc16a61f0d8aed7
Emerson Espinoza <xinayespinoza@gmail.com>
2023-05-07 18:14:28 -0400

    55.1 deploy en railway

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "vendelo-production-7696.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
